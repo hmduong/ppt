69233c2647280f9224cc90aa403e82ba47bcc1aa
hmduong <hmduongwb14@gmail.com>
2022-09-26 09:14:00 +0700

    update

--- src/js/bisection.js
+++ src/js/bisection.js
@@ -8,8 +8,8 @@ export default function bisection(form, a, b, step = 0) {
     if (fa * fb > 0) form.result.push({err: 'f(a) và f(b) cùng dấu'}); 
     else if (step <= form.p) {
         if (fmid === 0) form.result.push({step: step, mid: mid, a: a, fa: fa, b: b, fb: fb, fmid: fmid}); else {
-            fa * fmid < 0? b = mid: a = mid;
             form.result.push({step: step, mid: mid, a: a, fa: fa, b: b, fb: fb, fmid: fmid});
+            fa * fmid < 0? b = mid: a = mid;
             bisection(form, a, b, ++step);
         }
     }
